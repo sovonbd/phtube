95b84e9e23a44cad55b9973c0ce3fa2d105e7169
sovonbd <sovonsdp@gmail.com>
2023-09-01 13:01:53 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -16,45 +16,65 @@ const displayCategories = (categories) => {
   });
 };
 
-const loadCategoryItem = async (id) => {
+let catID;
+
+const loadCategoryItem = async (id = "1000") => {
   const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
   const data = await response.json();
   const categoryId = data.data;
+  catID = id;
   displayItem(categoryId);
 };
 
 const displayItem = (items) => {
   const itemContainer = document.getElementById("item-container");
   itemContainer.innerHTML = "";
+
   if (items.length !== 0) {
     items.forEach((item) => {
       const div = document.createElement("div");
-      div.classList = `card flex flex-col`;
+      div.classList = `card flex flex-col relative`;
       div.innerHTML = `
-      <figure><img class="h-48 w-full rounded-lg" src="${item.thumbnail}" alt="Shoes" /></figure>
-      <div class="flex flex-row gap-3 pt-4">
-        <img src="${item.authors[0]?.profile_picture}" class="rounded-full w-10 h-10" alt="" />
-        <div class="">
-          <h2 class="text-[#171717] text-base font-bold">${item.title}</h2>
-          <p class="text-[#171717B2] text-sm font-normal py-2">${item.authors[0]?.profile_name}<span class="ml-2 text-blue-600">${
-        item.authors[0]?.verified ? `<i class="fa-solid fa-certificate"></i>` : ""
+        <figure><img class="h-48 w-full rounded-lg" src="${item.thumbnail}" alt="Shoes" /></figure>
+        ${
+          item.others?.posted_date
+            ? `<span class="bg-[#171717] p-1 rounded-lg text-[10px] font-normal text-white absolute  bottom-[104px] right-3">${Math.floor(item.others.posted_date / 3600)} hrs ${Math.floor(
+                (item.others.posted_date % 3600) / 60
+              )} mins ago</span>`
+            : ""
+        }
+
+        <div class="flex flex-row gap-3 pt-4">
+          <img src="${item.authors[0]?.profile_picture}" class="rounded-full w-10 h-10" alt="" />
+          <div class="">
+            <h2 class="text-[#171717] text-base font-bold">${item.title}</h2>
+            <p class="text-[#171717B2] text-sm font-normal py-2 w-full flex flex-row gap-1">${item.authors[0]?.profile_name}<span>${
+        item.authors[0]?.verified ? `<img src="./images/verified.png" />` : ""
       }</span></p>
-          <p class="text-[#171717B2] text-sm font-normal">views: ${item.others?.views}</p>
+            <p class="text-[#171717B2] text-sm font-normal">views: ${item.others?.views}</p>
+          </div>
         </div>
-      </div>
-      `;
+        `;
       itemContainer.appendChild(div);
     });
   } else {
     const div = document.createElement("div");
     div.classList = `text-red-500 col-span-4 py-20 text-center`;
     div.innerHTML = `
-    <img class="text-center mx-auto rounded-lg w-28 h-32 pb-8" src="./images/icon.png" alt="Shoes" />
-    <h2 class="text-[#171717] text-3xl font-bold w-[433px] mx-auto">Oops!! Sorry, There is no content here</h2>
-    `;
+      <img class="text-center mx-auto rounded-lg w-28 h-32 pb-8" src="./images/icon.png" alt="Shoes" />
+      <h2 class="text-[#171717] text-3xl font-bold w-full md:w-[430px] mx-auto">Oops!! Sorry, There is no content here</h2>
+      `;
     itemContainer.appendChild(div);
   }
 };
 
+const sortByViews = async () => {
+  const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${catID}`);
+  const data = await response.json();
+  const categoryIdData = data.data;
+  const sortData = categoryIdData.sort((a, b) => parseFloat(b.others.views) - parseFloat(a.others.views));
+  displayItem(sortData);
+};
+
 loadCategories();
-loadCategoryItem(1000);
+loadCategoryItem();
